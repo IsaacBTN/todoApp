dc52817344b31024b302c54a78cbe28ff8764f07
Isaac Bertonha <isaacbertonha2003@gmail.com>
2025-03-12 01:51:23 -0300

    Criação da entidade Task e da classe TaskStatus

---
+++ todoApp/src/main/java/br/com/todo/todoApp/entities/Task.java
@@ -0,0 +1,54 @@
+package br.com.todo.todoApp.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Task() {
+    }
+
+    public Task(Long id, String name, TaskStatus status, User user) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ todoApp/src/main/java/br/com/todo/todoApp/entities/TaskStatus.java
@@ -0,0 +1,6 @@
+package br.com.todo.todoApp.entities;
+
+public enum TaskStatus {
+    PENDING,
+    COMPLETED
+}
